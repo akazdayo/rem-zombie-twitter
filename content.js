9d4b61510138603e7a70a4d3d0eaa3a1e9d9e432
akaz <82073147+akazdayo@users.noreply.github.com>
2024-01-30 19:52:01 +0900

    リファクタリングと、バグ修正

--- content.js
+++ content.js
@@ -1,45 +1,40 @@
-const spamText = ".";
-const newElement = document.createElement("div");
-let readedTweet = 0;
-newElement.innerHTML = '<p>スパムツイートを見つけました</p>';
-
-
 window.addEventListener("load", main, false);
 
+class Detect {
+	constructor() {
+		this.readedTweet = 0;
+		this.spamText = "Block Message";
+		this.deleteTweet = this.deleteTweet.bind(this);
+	}
+
+	checkTweet(tweet) {
+		if (tweet.textContent.includes(this.spamText)) {
+			console.log(tweet.textContent);
+			return true;
+		}
+		return false;
+	}
+
+	deleteTweet() {
+		const tweets = document.querySelectorAll('[data-testid="tweetText"]');
+
+		for (let i = 0; i < tweets.length; i++) {
+			if (this.checkTweet(tweets[i]) === true) {
+				console.log("ツイートを削除しました");
+				tweets[i].parentNode.removeChild(tweets[i]);
+			}
+		}
+		//this.readedTweet = tweets.length;
+		console.log(tweets);
+		console.log(tweets.length, "個のツイートを精査しました。");
+	}
+
+	start() {
+		setInterval(this.deleteTweet, 500);
+	}
+}
 
 function main(e) {
-    console.log("Hello, World!");
-    const jsInitCheckTimer = setInterval(jsLoaded, 3000);
-
-    function getAllTweet() {
-        // XPathを使用して要素を取得
-        const xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div";
-        const iterator = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-
-        return iterator.iterateNext();
-    }
-
-    function jsLoaded() {
-        // ツイートを取得
-        const elements = document.getElementsByClassName('css-1rynq56 r-8akbws r-krxsd3 r-dnmrzs r-1udh08x r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-bnwqim');
-        //console.log("elements: ", elements);
-        if (elements !== null){
-            const allTweet = getAllTweet();
-            //clearInterval(jsInitCheckTimer); 一旦コメントアウト
-            // 取得した要素のテキスト内容をすべて表示
-            for (let i = readedTweet; i < elements.length; i++) {
-                console.log("elements: ", elements[i].textContent);
-                if (elements[i].textContent.includes(spamText)) {
-                    console.log("スパムツイートを見つけました");
-                    // ツイートを削除
-                    //allTweet.removeChild(allTweet.childNodes[i]);
-                    //allTweet.replaceChild(newElement, allTweet.childNodes[i]);
-
-                    elements[i].parentNode.removeChild(elements[i]);
-                    //elements[i].parentNode.replaceChild(newElement, elements[i]);
-                }
-            }
-            readedTweet = elements.length;
-        }
-    };
+	const detect = new Detect();
+	detect.start();
 }
