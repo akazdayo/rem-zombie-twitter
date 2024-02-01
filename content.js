1b0eacf3b774e6ae237392a738254c889e97d75c
akaz <82073147+akazdayo@users.noreply.github.com>
2024-02-01 18:04:45 +0900

    Update content.js and manifest.json

--- content.js
+++ content.js
@@ -1,18 +1,30 @@
+const sleep = (waitTime) =>
+	new Promise((resolve) => setTimeout(resolve, waitTime));
 window.addEventListener("load", main, false);
 
 class Detect {
 	constructor() {
 		this.readedTweet = 0;
-		this.spamText = "Block Message";
+		this.spamText = "Context";
 		this.deleteTweet = this.deleteTweet.bind(this);
 	}
 
 	checkTweet(tweet) {
+		if (tweet === null || tweet === undefined) {
+			console.log("Tweet is null or undefined");
+			return true;
+		}
 		if (tweet.innerText.includes(this.spamText)) {
-			console.log(tweet.textContent);
 			return true;
 		}
 		return false;
+	}
+
+	createDummyTweet(tweet) {
+		const dummyTweet = document.createElement("div");
+		dummyTweet.setAttribute("data-testid", "cellInnerDiv");
+		dummyTweet.setAttribute("style", tweet.getAttribute("style"));
+		return dummyTweet;
 	}
 
 	deleteTweet() {
@@ -20,6 +32,7 @@ class Detect {
 		const tweetBorders = document.querySelectorAll(
 			'[data-testid="cellInnerDiv"]',
 		);
+		const allTweet = [];
 
 		for (let i = 0; i < tweetBorders.length; i++) {
 			let nowTweet;
@@ -27,19 +40,27 @@ class Detect {
 				nowTweet =
 					tweetBorders[i].children[0].children[0].children[0].children[0]
 						.children[0].children[1].children[1].children[1].children[0];
+				nowTweet.innerText;
 			} catch (e) {
 				continue;
 			}
 
-			if (this.checkTweet(nowTweet) === true) {
-				//nowTweet[i].parentNode.removeChild(nowTweet[i]);
-				tweetBorders[i].parentNode.removeChild(tweetBorders[i]);
+			if (
+				this.checkTweet(nowTweet) === true ||
+				allTweet.includes(nowTweet.innerText) === true
+			) {
+				console.log("Tweet deleted: ", nowTweet);
+				const newElement = this.createDummyTweet(tweetBorders[i]);
+				sleep(3000).then(() => {
+					tweetBorders[i].parentNode.replaceChild(newElement, tweetBorders[i]);
+				});
 			}
+			allTweet.push(nowTweet.innerText);
 		}
 	}
 
 	start() {
-		setInterval(this.deleteTweet, 1500);
+		setInterval(this.deleteTweet, 3000);
 	}
 }
 
